3c82a5822ee063075c25803a05793e01abc1ec33
shadreza <shadreza100@gmail.com>
2023-02-17 18:51:40 +0600

    create done

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Models\Post;
+use App\Models\Tag;
+use App\Models\Video;
+use App\Models\Taggable;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +19,27 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+
+
+/*
+|--------------------------------------------------------------------------
+| CRUD
+|--------------------------------------------------------------------------
+*/
+
+
+// create
+// when any tag is tagged to any content [video / post] -> taggable table will be populated as that is the pivot table
+
+Route::get('create', function () {
+    $post = Post::create(['name' => 'Post 1']);
+    $tag = Tag::findOrFail(1);
+    $post->tags()->save($tag);
+
+    $video = Video::create(['name' => 'Video 1']);
+    $tag = Tag::findOrFail(2);
+    $video->tags()->save($tag);
 });
